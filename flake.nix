c2dc811c08dd2d45fbbcbe94559bd526db9345f4
Michael Pontus <m.pontus@gmail.com>
2022-03-25 19:24:27 +0300

    chore(flake): make build independent from non-application files

--- flake.nix
+++ flake.nix
@@ -4,16 +4,39 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    nix-filter.url = "github:numtide/nix-filter";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, nix-filter }:
     flake-utils.lib.eachDefaultSystem (system:
       with import nixpkgs { inherit system; };
-      let project = pkgs.callPackage ./yarn-project.nix { } { src = ./.; };
+      let
+        project = pkgs.callPackage ./yarn-project.nix { } {
+          src = nix-filter.lib {
+            root = ./.;
+            include = (map nix-filter.lib.inDirectory [
+              ".yarn"
+              "plugins"
+              "src"
+              "static"
+            ]) ++ [
+              ".yarnrc.yml"
+              "gatsby-browser.js"
+              "gatsby-config.js"
+              "gatsby-node.js"
+              "gatsby-ssr.js"
+              "graphql-types.ts"
+              "package.json"
+              "postcss.config.js"
+              "tsconfig.json"
+              "yarn.lock"
+            ];
+          };
+        };
       in rec {
         defaultPackage = project.overrideAttrs (oldAttrs: {
-          buildInputs = oldAttrs.buildInputs ++ [ pkgs.python3 ];
-          buildPhase = "yarn build";
+          buildInputs = oldAttrs.buildInputs ++ [ pkgs.tree pkgs.python3 ];
+          buildPhase = "tree src; yarn build";
           installPhase = "mv public $out";
         });
 
