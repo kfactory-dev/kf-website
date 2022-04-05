637a7ff1d2cc82f214d7fca61c83bc4b29e9db4e
Michael Pontus <m.pontus@gmail.com>
2022-04-06 00:47:05 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 const config = {
   siteMetadata: {
     siteUrl: `https://kf-web.netlify.app`,
-    title: `kf-website`,
-    titleTemplate: `%s - kf-website`,
+    title: `k/factory`,
+    titleTemplate: `%s - k/factory`,
     description:
       'k/factory is a software studio building open source software powering the financial system of tomorrow',
   },
